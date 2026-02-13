dbc4135b2f30888d0bf6b344aa1217614246fadd
gus <tadolfo_va@hotmail.com>
2026-02-12 20:16:15 -0500

    solucion al problema1

--- problema1/cliente.py
+++ problema1/cliente.py
@@ -6,18 +6,32 @@ Objetivo: Crear un cliente TCP que se conecte a un servidor e intercambie mensaj
 
 import socket
 
-# TODO: Crear un socket TCP/IP
+HOST = "localhost"
+PORT = 9000
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
+
+cliente.sendall(b"Mundo!")
+respuesta = cliente.recv(1024)
+print(f"Respuesta: {respuesta}")
+
+cliente.close()
+
+
+
+# : Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
 
-# TODO: Conectar el socket al servidor en la dirección y puerto especificados
+# : Conectar el socket al servidor en la dirección y puerto especificados
 
-# TODO: Enviar datos al servidor (convertidos a bytes)
+# : Enviar datos al servidor (convertidos a bytes)
 # sendall() asegura que todos los datos sean enviados
 
-# TODO: Recibir datos del servidor (hasta 1024 bytes)
+# : Recibir datos del servidor (hasta 1024 bytes)
 
-# TODO: Decodificar e imprimir los datos recibidos
+# : Decodificar e imprimir los datos recibidos
 
-# TODO: Cerrar la conexión con el servidor
+# : Cerrar la conexión con el servidor
 
--- problema1/servidor.py
+++ problema1/servidor.py
@@ -6,30 +6,44 @@ Objetivo: Crear un servidor TCP que acepte una conexión y intercambie mensajes 
 
 import socket
 
-# TODO: Definir la dirección y puerto del servidor
+#: Definir la dirección y puerto del servidor
+HOST = "localhost"
+PORT = 9000
 
-# TODO: Crear un socket TCP/IP
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORT))
+servidor.listen()
+print("el servidor esta a la espera de conexiones ...")
+
+cliente, direccion =servidor.accept()
+print(f"un cliente se conecto desde la direccion {direccion}")
+
+datos = cliente.recv(1024)
+cliente.sendall(b"Hola! " + datos) # ojo! debe ser binario, no cadena
+cliente.close()
+
+#: Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
 
-# TODO: Enlazar el socket a la dirección y puerto especificados
+# : Enlazar el socket a la dirección y puerto especificados
 
-# TODO: Poner el socket en modo escucha
+# : Poner el socket en modo escucha
 # El parámetro define el número máximo de conexiones en cola
 
-print("Servidor a la espera de conexiones ...")
 
-# TODO: Aceptar una conexión entrante
+
+# : Aceptar una conexión entrante
 # accept() bloquea hasta que llega una conexión
 # conn: nuevo socket para comunicarse con el cliente
 # addr: dirección y puerto del cliente
 
-print(f"Conexión realizada por {addr}")
 
-# TODO: Recibir datos del cliente (hasta 1024 bytes)
+
+# : Recibir datos del cliente (hasta 1024 bytes)
  
-# TODO: Enviar respuesta al cliente (convertida a bytes)
+# : Enviar respuesta al cliente (convertida a bytes)
 # sendall() asegura que todos los datos sean enviados
 
-# TODO: Cerrar la conexión con el cliente
+# : Cerrar la conexión con el cliente
 
